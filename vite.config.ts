a5b636292fa2ce9caca8fbc88213016c5cd0a0b0
DUMA042 <dumebiikeoffiah@gmail.com>
2026-08-03 10:42:10 +0100

    For now working UI dashboard next commit should fix more thing in the UI

--- vite.config.ts
+++ vite.config.ts
@@ -22,7 +22,7 @@ export default defineConfig(async () => ({
     watch: { ignored: ["**/src-tauri/**"] },
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:8000",
+        target: "http://127.0.0.1:8001",
         changeOrigin: true,
       },
     },
